f6298217976df3a6ec7e6929b3def7ae54f1ebb8
Vlad Lopes <vlad@vladlopes.com.br>
2021-02-16 18:49:01 -0300

    Ajustes Finais

--- TesteiOSv2/AccountScene/AccountScenePresenter.swift
+++ TesteiOSv2/AccountScene/AccountScenePresenter.swift
@@ -32,10 +32,10 @@ class AccountScenePresenter: AccountScenePresentationLogic
         formatador.locale = Locale(identifier: "pt_BR")
         formatador.numberStyle = .currency
 
+        // MARK: Bank account and agency has these contents changed in example
         var agency = response.client.agency
         agency.insert(".", at: agency.index(agency.startIndex, offsetBy: 2))
         agency.insert("-", at: agency.index(agency.startIndex, offsetBy: 8))
-
 
         let name = response.client.name
         let bankAccount = "\(response.client.bankAccount) / \(agency) "
@@ -55,6 +55,7 @@ class AccountScenePresenter: AccountScenePresentationLogic
             let account = AccountData(title: sta.title, date: sta.date, description: sta.desc, value: String(value))
             accounts.append(account)
         }
+        
         let viewModel = AccountScene.Statements.ViewModel(statements: accounts)
         viewController?.displayStatements(viewModel: viewModel)
     }
